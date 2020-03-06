98ed670e4f6f95126c504319028754706087e3fa
Dpalme <diegoPalmerin24@gmail.com>
2020-03-05 22:12:22 -0600

    replaced lorem ipsums with actual text

--- scraper.py
+++ scraper.py
@@ -1,18 +1,24 @@
-import pyperclip
+# encoding=UTF-8
+import pyperclip, pprint
 
 cadena_final = ""
-with open("input.txt", "r", encoding="UTF-8") as archivo:
+with open("committees.txt", "r", encoding="UTF-8") as archivo:
   for line in archivo.readlines():
-    abreviation = ""
-    name = ""
-    cadena_final += """
-    <div class="committee blue-bg">
-      <div class="absolute-centered fourth-width">
-        <h4 class="white">%s</h4>
-        <div class="division-line mid-white-bg"></div>
-        <p class="white small-body">%s</p>
+    if "*" in line:
+      pass
+    else:
+      name, abreviation, idioma = line.split(";")
+      cadena_final += """
+      <div class="flex-item relative">
+        <div class="committee green-bg relative">
+          <div class="absolute-centered full-width-percent">
+            <h4 class="white full-width-percent">%s</h4>
+            <div class="division-line mid-white-bg"></div>
+            <p class="white small-body">%s</p>
+          </div>
+        </div>
       </div>
-    </div>
-    """ % (abreviation, name)
+      """ % (abreviation, name)
 
+pprint.pprint(cadena_final)
 pyperclip.copy(cadena_final)
